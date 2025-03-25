39055ec2e87ea08bc0751486e7a9417cedc029cd
Sangram Thapa <thapasangram64@gmail.com>
2025-03-25 10:42:02 +0545

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDom.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
       <App />
-    </StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
