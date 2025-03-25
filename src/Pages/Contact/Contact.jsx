0e2266b42a68d883e2852fd5b400c68830e39634
Sangram Thapa <thapasangram64@gmail.com>
2025-03-25 11:25:11 +0545

    added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,12 +1,22 @@
-import { useContext } from "react";
-import { NewContext } from "../Context/Contextsss";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
 
+const getFunction = async () => {
+  const { data } = await axios.get("https://fakestoreapi.com/products");
+  return data;
+};
 function Contact() {
-  const { state, setState } = useContext(NewContext);
+  const { data } = useQuery({ queryKey: ["id"], queryFn: getFunction });
   return (
-    <div className="mt-30">
-      {state}
-      <button onClick={() => setState(state + 1)}>CLick</button>
+    <div className="max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 md:grids-cols-2 lg:grid-cols-3 p-10 gap-10">
+        {data?.map((item) => (
+          <div key={item.id}>
+            <img src={item.image} className="h-40 w-40 "></img>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import ReactDom from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
-ReactDom.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <App />
+      <StrictMode>
+        <App />
+      </StrictMode>
     </BrowserRouter>
   </QueryClientProvider>
 );
